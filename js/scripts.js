daa536871ee695a075de180e70c80159751bfff0
robajillo <rjillo@yahoo.com>
2020-07-29 18:23:55 +0300

    added an hover effect on my portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,75 @@ $(document).ready(function() {
     $(".whatwedo3").toggle();
   });
 
+  $(document).ready(function () {
+      $(".myportfolio").hover(
+          function () {
+              $(this)
+                  .find(".overlay")
+                  .show();
+          },
+          function () {
+              $(this)
+                  .find(".overlay")
+                  .hide();
+          }
+      );
+  });
+  $(".one").hover(function () {
+        $(".overlay1").show();
+    });
+    $(".one").mouseleave(function () {
+        $(".overlay1").hide();
+    });
+    $(".two").hover(function () {
+        $("#overlay2").show();
+    });
+    $(".two").mouseleave(function () {
+        $("#overlay2").hide();
+    });
+    $(".three").hover(function () {
+        $("#overlay3").show();
+    });
+    $(".three").mouseleave(function () {
+        $("#overlay3").hide();
+    });
+    $(".four").hover(function () {
+        $("#overlay4").show();
+    });
+    $(".four").mouseleave(function () {
+        $("#overlay4").hide();
+    });
+    $(".five").hover(function () {
+        $("#overlay5").show();
+    });
+    $(".five").mouseleave(function () {
+        $("#overlay5").hide();
+    });
+    $(".six").hover(function () {
+        $("#overlay6").show();
+    });
+    $(".six").mouseleave(function () {
+        $("#overlay6").hide();
+    });
+    $(".seven").hover(function () {
+        $("#overlay7").show();
+    });
+    $(".seven").mouseleave(function () {
+        $("#overlay7").hide();
+    });
+    $(".eight").hover(function () {
+        $("#overlay8").show();
+    });
+    $(".eight").mouseleave(function () {
+        $("#overlay8").hide();
+    });
+    event.preventDefault();
+});
+
+
+
+
+
 
 
   $("form").submit(function() {
@@ -31,9 +100,4 @@ $(document).ready(function() {
 
   });
 
-  $('.portimg').hover(function(){
-    $('.porttext',this).slideToggle('slow');
- }, function(){
-    $('.porttext',this).slideToggle('slow');
- });
-});
+  
